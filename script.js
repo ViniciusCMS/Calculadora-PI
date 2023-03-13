060f1eadb0c1cdc266a5fc2c6872c87782f589ed
Vinicius Martins <93468768+ViniciusCMS@users.noreply.github.com>
2023-03-13 09:30:06 -0300

    Salve 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
         result.value = number + num;
     }
 
-// Calculator logic
+// Lógica para calculadora
     function calc(){
         if(result.value != ""){
             let result2  = result.value;
@@ -19,12 +19,12 @@
         }
     }
 
-// Reset button
+// Botão RESET 
     function reset(){
         result.value = "";
     }
 
-// Del button
+// Botão DELETE (del)
     function del(){
         let result2  = result.value;
         result.value = result2.substring(0, result2.length - 1);
@@ -33,8 +33,8 @@
     // Todas cores de estilo
         const theme = {
             defaul(){
-                root.style.setProperty('--background'          , '#3a4764');
-                root.style.setProperty('--background-dark'     , '#232c43');
+                root.style.setProperty('--background'          , '#92B75D');
+                root.style.setProperty('--background-dark'     , '#00A987');
                 root.style.setProperty('--background-very-dark', '#182034');
                 
                 root.style.setProperty('--key-color-top'       , '#ffffff');
@@ -50,8 +50,8 @@
                 root.style.setProperty('--key-red-shadow'      , '#93261a');
             },
             light(){
-                root.style.setProperty('--background'          , '#e6e6e6');
-                root.style.setProperty('--background-dark'     , '#d3cdcd');
+                root.style.setProperty('--background'          , '#C9EF91');
+                root.style.setProperty('--background-dark'     , '#E5FFF7');
                 root.style.setProperty('--background-very-dark', '#eeeeee');
                 
                 root.style.setProperty('--key-color-top'       , '#3d3d33');
@@ -67,9 +67,9 @@
                 root.style.setProperty('--key-red-shadow'      , '#93261a');
             },
             dark(){
-                root.style.setProperty('--background'          , '#17062a');
-                root.style.setProperty('--background-dark'     , '#1e0836');
-                root.style.setProperty('--background-very-dark', '#1e0836');
+                root.style.setProperty('--background'          , '#007562');
+                root.style.setProperty('--background-dark'     , '#245161');
+                root.style.setProperty('--background-very-dark', '#245161');
                 
                 root.style.setProperty('--key-color-top'       , '#f7de43');
                 root.style.setProperty('--key-color-bottom'    , '#f7de43');
@@ -84,14 +84,14 @@
             }
         }
 
-// Get the root element
+// Integração de variavais/root
     var root = document.querySelector(':root');
 
-// Checking the user themes preference
+// Verificar presença de estilo
     const darkThemeMq  = window.matchMedia("(prefers-color-scheme: dark)");
     const lightThemeMq = window.matchMedia("(prefers-color-scheme: light)");
 
-// changing the theme with the results above
+// Mudança de temas adquiridos
     if (darkThemeMq.matches) {
         document.getElementById('btnTheme').value = "3";
         theme.dark();
@@ -103,23 +103,23 @@
         theme.defaul();    
     }
 
-// Create a function for recive the value of range input
+// Criação de funções adquiridas
     function myFunction_set(val) {
         // receiving the input type range value
-        document.getElementById('btnTheme').value = val; 
+            document.getElementById('btnTheme').value = val; 
 
-        // changing the theme with the results above
-        if(val == 1){
-            theme.defaul();
-        } 
-        
-        else if(val == 2){
-            theme.light();
-        }
-        
-        else{
-            theme.dark();
-        } 
+            // changing the theme with the results above
+            if(val == 1){
+                theme.defaul();
+            } 
+            
+            else if(val == 2){
+                theme.light();
+            }
+            
+            else{
+                theme.dark();
+            } 
     
     }
 
